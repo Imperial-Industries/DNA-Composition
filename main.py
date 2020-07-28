e584ce9d8e983d22011b4aeb74962a0cea9f7954
Imperial Industries <33400784+Imperial-Industries@users.noreply.github.com>
2020-07-28 17:31:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 homo_sapiens = open('hsapiens.txt').readlines()[0]
 plasmodium_falciparum = open('pfalciparum.txt').readlines()[0]
 common_mosquito = open('agambiaeas.txt').readlines()[0]
+coliphage_phix174 = open('coliphage_phi-x174.txt').readlines()[0]
 # Determining the length of the sequence
 def length(string):
 	length = 0;
@@ -35,15 +36,21 @@ print("Adenine Count: " + str(adenine_count(homo_sapiens)))
 print("Cytosine Count: " + str(cytosine_count(homo_sapiens)))
 print("Guanine Count: " + str(guanine_count(homo_sapiens)))
 print("###")
-print("Length: " + str(length(homo_sapiens)))
+print("Length: " + str(length(plasmodium_falciparum)))
 print("Thymine Count: " + str(thymine_count(plasmodium_falciparum)))
 print("Adenine Count: " + str(adenine_count(plasmodium_falciparum)))
 print("Cytosine Count: " + str(cytosine_count(plasmodium_falciparum)))
 print("Guanine Count: " + str(guanine_count(plasmodium_falciparum)))
 print("###")
-print("Length: " + str(length(homo_sapiens)))
+print("Length: " + str(length(common_mosquito)))
 print("Thymine Count: " + str(thymine_count(common_mosquito)))
 print("Adenine Count: " + str(adenine_count(common_mosquito)))
 print("Cytosine Count: " + str(cytosine_count(common_mosquito)))
 print("Guanine Count: " + str(guanine_count(common_mosquito)))
+print("###")
+print("Length: " + str(length(coliphage_phix174)))
+print("Thymine Count: " + str(thymine_count(coliphage_phix174)))
+print("Adenine Count: " + str(adenine_count(coliphage_phix174)))
+print("Cytosine Count: " + str(cytosine_count(coliphage_phix174)))
+print("Guanine Count: " + str(guanine_count(coliphage_phix174)))
 print("###")
